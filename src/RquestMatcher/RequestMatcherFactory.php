3092c8ef7751c7f6fbb22d6b2ee4b1ab71c8ac33
liuzhanpeng <liuzhanpeng@gmail.com>
2025-05-21 18:46:43 +0800

    整理文件结构

--- src/RquestMatcher/RequestMatcherFactory.php
+++ src/RquestMatcher/RequestMatcherFactory.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Lzpeng\HyperfAuthGuard\ServiceFactory;
+namespace Lzpeng\HyperfAuthGuard\RquestMatcher;
 
 use Hyperf\Contract\ContainerInterface;
 use Lzpeng\HyperfAuthGuard\Config\CustomConfig;
--- src/UnauthenticatedHandler/UnauthenticatedHandlerFactory.php
+++ src/UnauthenticatedHandler/UnauthenticatedHandlerFactory.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Lzpeng\HyperfAuthGuard\ServiceFactory;
+namespace Lzpeng\HyperfAuthGuard\UnauthenticatedHandler;
 
 use Hyperf\Contract\ContainerInterface;
 use Lzpeng\HyperfAuthGuard\Config\CustomConfig;
