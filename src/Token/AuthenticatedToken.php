907632ef777c6d5d198018f71f97eebf593a6729
liuzhanpeng <liuzhanpeng@gmail.com>
2026-01-21 09:12:09 +0800

    删除AbstractToken

--- src/Token/AuthenticatedToken.php
+++ src/Token/AuthenticatedToken.php
@@ -11,8 +11,88 @@ namespace GaaraHyperf\Token;
  * 
  * @author lzpeng <liuzhanpeng@gmail.com>
  */
-class AuthenticatedToken extends AbstractToken
+class AuthenticatedToken implements TokenInterface
 {
+    /**
+     * @param string $guardName
+     */
+    public function __construct(
+        protected string $guardName,
+        protected string $userIdentifier,
+        protected array $attributes = []
+    ) {}
+
+    /**
+     * 返回认证守卫名称
+     *
+     * @return string
+     */
+    public function getGuardName(): string
+    {
+        return $this->guardName;
+    }
+
+    /**
+     * 返回用户标识
+     *
+     * @return string
+     */
+    public function getUserIdentifier(): string
+    {
+        return $this->userIdentifier;
+    }
+
+    /**
+     * 是否存在属性
+     *
+     * @param string $name
+     * @return boolean
+     */
+    public function hasAttribute(string $name): bool
+    {
+        return array_key_exists($name, $this->attributes);
+    }
+
+    /**
+     * 返回指定属性
+     *
+     * @param string $name
+     * @return mixed
+     */
+    public function getAttribute(string $name): mixed
+    {
+        if (!array_key_exists($name, $this->attributes)) {
+            throw new \InvalidArgumentException(sprintf('This token has no "%s" attribute.', $name));
+        }
+
+        return $this->attributes[$name];
+    }
+
+    /**
+     * 设置属性
+     *
+     * @param string $name
+     * @param mixed $value
+     * @return void
+     */
+    public function setAttribute(string $name, mixed $value): void
+    {
+        $this->attributes[$name] = $value;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function __toString(): string
+    {
+        return sprintf(
+            '%s(user=%s, attributes=%s)',
+            static::class,
+            $this->userIdentifier,
+            json_encode($this->attributes)
+        );
+    }
+
     /**
      * @return array
      */
--- src/Token/TokenInterface.php
+++ src/Token/TokenInterface.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace GaaraHyperf\Token;
 
-use GaaraHyperf\User\UserInterface;
-
 /**
  * 用户令牌接口
  * 
